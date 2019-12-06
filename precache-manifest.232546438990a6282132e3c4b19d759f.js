6666f8b1f7fcd9feaeccda503c53fc2deaef68e2
Your Name <you@example.com>
2019-12-06 12:52:32 +0100

    Updates

---
+++ precache-manifest.232546438990a6282132e3c4b19d759f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca41e170b7385e305280ff96f8f54786",
+    "url": "/lScheduleAppPulished/index.html"
+  },
+  {
+    "revision": "b192ea82bec071944ad2",
+    "url": "/lScheduleAppPulished/static/css/main.9abce887.chunk.css"
+  },
+  {
+    "revision": "7db240dc7f9a4898c7f1",
+    "url": "/lScheduleAppPulished/static/js/2.dfd7cbca.chunk.js"
+  },
+  {
+    "revision": "b192ea82bec071944ad2",
+    "url": "/lScheduleAppPulished/static/js/main.5b88a937.chunk.js"
+  },
+  {
+    "revision": "2b9bdad7c62d49671da0",
+    "url": "/lScheduleAppPulished/static/js/runtime-main.500b8abd.js"
+  }
+]);
