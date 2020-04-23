519d1ad26202b86c5f5f7c815a39d8554b0d2959
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-04-23 12:12:40 +0200

    Updates

--- precache-manifest.80f1ee39597f154c82edb468953c4d3e.js
+++ precache-manifest.80f1ee39597f154c82edb468953c4d3e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "452bdd158aa981e2d6b4dab81edd86e8",
+    "revision": "2c2d1675e5a26eff73c7a976a02614aa",
     "url": "/ScheduleApp/index.html"
   },
   {
-    "revision": "04a037310737391733e2",
+    "revision": "6f302b42ec90df7d92d8",
     "url": "/ScheduleApp/static/css/main.d4b7dafd.chunk.css"
   },
   {
-    "revision": "9dfdc2afed529224e161",
-    "url": "/ScheduleApp/static/js/2.94dfb8aa.chunk.js"
+    "revision": "7e5fad285c4feae341c8",
+    "url": "/ScheduleApp/static/js/2.d1d51362.chunk.js"
   },
   {
     "revision": "b289ee6f6b4327761591c764998a9dea",
-    "url": "/ScheduleApp/static/js/2.94dfb8aa.chunk.js.LICENSE"
+    "url": "/ScheduleApp/static/js/2.d1d51362.chunk.js.LICENSE"
   },
   {
-    "revision": "04a037310737391733e2",
-    "url": "/ScheduleApp/static/js/main.25f3dab4.chunk.js"
+    "revision": "6f302b42ec90df7d92d8",
+    "url": "/ScheduleApp/static/js/main.e1eee196.chunk.js"
   },
   {
     "revision": "2e8c77fc6903606ef989",
