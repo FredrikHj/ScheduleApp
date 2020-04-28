8262ecc2e4fa09328ef2e548ef449110cae8e4d3
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-04-28 23:48:32 +0200

    Fixing with cellAddForms,  done 95%

--- src/Components/Structure/ListAddForm.js
+++ src/Components/Structure/ListAddForm.js
@@ -4,6 +4,7 @@ import { SubmitBtn } from '../Data/SubmitBtn';
 import { axiosPost } from '../Data/Axios';
 import { CellInput } from '../Data/CellInput';
 import { optionColListArr$ } from '../Storage';
+import Calendar from 'react-calendar';
 
 export let ListAddForm = (props) => {
     //let [ incommingNewSQLData, updateIncommingNewSQLData ] = useState([]);
@@ -15,19 +16,20 @@ export let ListAddForm = (props) => {
     useEffect(() => {
         optionColListArr$.subscribe((SQLColListArr) => {
             // It can handle the data an perform its task regardless the data infrastructure 
-            if (SQLColListArr) if(structuredSQLDataArr.length === 0) updateStructuredSQLDataArr(SQLColListArr);
+            if (SQLColListArr) if(structuredSQLDataArr.length === 0) updateStructuredSQLDataArr(Object.values(SQLColListArr));
             //if (SQLDataArr.statusText === 'OK') updateIncommingNewSQLData(SQLDataArr.data[0]);
         });
         createAddedRecordDataArr();
     }, [addedRecordData]);
     const createAddedRecordDataArr = () => {
         const puschToAddedRecordData = [...addedRecordData];
-        
-        structuredSQLDataArr.filter((item, index) => puschToAddedRecordData.push(''));
+        structuredSQLDataArr.filter((item, index) => {
+            if(index === 0) puschToAddedRecordData.push('20YY-MM-DD');
+            else puschToAddedRecordData.push('');
+        });
         if (addedRecordData.length === 0) updateAddedRecordData(puschToAddedRecordData);
-        console.log("createAddedRecordDataArr -> addedRecordData", addedRecordData)
-
     }    
+    console.log("ListAddForm -> structuredSQLDataArr", structuredSQLDataArr)
     let setStrsType = (e) => {
         const puschToAddedRecordData = [...addedRecordData];
         let type = e.target;
@@ -38,7 +40,7 @@ export let ListAddForm = (props) => {
         updateAddedRecordData(puschToAddedRecordData);
         
     }
-        const chooseSelectOption = (e) => {
+        const choosenSelectOption = (e) => {
         const puschToAddedRecordData = [...addedRecordData];
         const selectedOption = e.target; 
         const selectedStr = selectedOption.value;
@@ -46,7 +48,6 @@ export let ListAddForm = (props) => {
         const selectedCellIndex = selectedOption.options[selectedIndex].dataset.cell;
         puschToAddedRecordData[selectedCellIndex] =selectedStr;
         updateAddedRecordData(puschToAddedRecordData);
-        console.log("setStrsType -> addedRecordData", addedRecordData)
     }
     const addCellData = (cellData) => {
         let pushToAddCellData = [...addCellData];
@@ -54,7 +55,7 @@ export let ListAddForm = (props) => {
     }
     let runAddRow = (e) => {
         const pushToAddedRecords = [...addedRecords];
-
+        
         // Gets the element
         let targetBtnId = e.target.id;     
         //let addedRecord = [0, dateStr, activityStr, stateStr, concernedStr, typeStr, placeStr, contentStr];
@@ -73,9 +74,13 @@ export let ListAddForm = (props) => {
         //console.log(sqlBody);
         
     }
+    const handlerDateCol = (e) => {
+
+    }
     const mappCols = (itemIndex) => {
         let saveToColsArr = [];
     }
+    console.log("ListAddForm -> structuredSQLDataArr", structuredSQLDataArr)
     return(
         <>
             <tr>
@@ -83,16 +88,35 @@ export let ListAddForm = (props) => {
                     tableHeadline.map((item, cellIndex) => {
                         return(
                             <td key={ cellIndex }>
-                                <CellInput 
-                                    dataType={ item }
-                                    cellIndex={ cellIndex }
-                                    inputOnChange={ setStrsType }
-                                    sOOnClick={ chooseSelectOption }
-                                    cellStr={ addedRecordData[cellIndex] }
-                                    valueStr={ addedRecordData[cellIndex] }
-                                    options={ structuredSQLDataArr[cellIndex] !== undefined && structuredSQLDataArr[cellIndex] }
+                                {(cellIndex === 0)
+                                    ? 
+                                        <>      
+                                            <input 
+                                                /* type="text"
+                                                style={ UserInputForm.UserInputForm }
+                                                data-type={ dataType } 
+                                                data-typenr={ cellIndex }
+                                                onChange={ inputOnChange } */ 
+                                                value={ addedRecordData[cellIndex] }
+                                            />
+                                            <Calendar
+                                                onChange={ handlerDateCol }
+                                                value={ '' }
+                                            />
+                                        </>
+                                    
+                                   
+                                    : <CellInput 
+                                        dataType={ item }
+                                        cellIndex={ cellIndex }
+                                        inputOnChange={ setStrsType }
+                                        sOOnClick={ choosenSelectOption }
+                                        cellStr={ addedRecordData[cellIndex] }
+                                        valueStr={ addedRecordData[cellIndex] }
+                                        options={ structuredSQLDataArr[cellIndex] !== undefined && structuredSQLDataArr[cellIndex] }
 
-                               />
+                                     />
+                               }
                             </td>
                         );
                     })
