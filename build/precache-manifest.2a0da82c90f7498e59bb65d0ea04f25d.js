e49a7b0b48948e60d4321852eb726634aa831b8a
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-05-27 09:56:16 +0200

    Pushed into master

---
+++ build/precache-manifest.2a0da82c90f7498e59bb65d0ea04f25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f304f7eecfa71ba41be0788c4bf71d",
+    "url": "/ScheduleApp/index.html"
+  },
+  {
+    "revision": "5b52696defa195155ea3",
+    "url": "/ScheduleApp/static/css/main.f3cc12ab.chunk.css"
+  },
+  {
+    "revision": "c295fd91bf5b2e7e8dbd",
+    "url": "/ScheduleApp/static/js/2.9c2f2289.chunk.js"
+  },
+  {
+    "revision": "b35c2ed9b3ab91d91ce72a49ee18374f",
+    "url": "/ScheduleApp/static/js/2.9c2f2289.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b52696defa195155ea3",
+    "url": "/ScheduleApp/static/js/main.1e4c42d5.chunk.js"
+  },
+  {
+    "revision": "2e8c77fc6903606ef989",
+    "url": "/ScheduleApp/static/js/runtime-main.02ff3cb5.js"
+  }
+]);
